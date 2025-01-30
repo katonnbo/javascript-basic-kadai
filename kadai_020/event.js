01ea13bfd8fe84ebcba193669bdb9685241219a7
katonnbo <katonbo02rr@gmail.com>
2025-01-30 22:54:41 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const  btnFrist = document.getElementById('btn');
+
+const idFrist = document.getElementById('text');
+
+btnFrist.addEventListener('click', () => {
+    idFrist.textContent = 'ボタンをクリックしました';
+});
